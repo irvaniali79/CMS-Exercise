7876ab1273d598a69989445af1c2c5700c24610b
okabe-rintaru <irvaniali79@gmail.com>
2021-03-13 22:57:41 +0330

    fixed creating table queries

--- CMS/admin-dashboard/CreateDB.php
+++ CMS/admin-dashboard/CreateDB.php
@@ -7,27 +7,27 @@ class CreateDB extends Database
 {
     private $createTableQueries=array(
         "CREATE TABLE `categories` (
-            `id` int(11) NOT NULL AUTO_INCRIMENT,
+            `id` int(11) NOT NULL AUTO_INCREMENT  ,
             `name` varchar(200) COLLATE utf8_persian_ci NOT NULL,
             `create_at` datetime NOT NULL,
-            `update_at` datetime DEFAULT,
-            PRIMARY KEY (`id`)
+            `update_at` datetime DEFAULT NULL,
+            PRIMARY KEY(`id`)
         )ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_persian_ci;",
         
         "CREATE TABLE `users` (
-            `id` int(11) NOT NULL AUTO_INCRIMENT,
-            `username` varchar(100) NOT NULL COLLATE utf8_persian_ci NOT NULL,
-            `email` varchar(100) NOT NULL COLLATE utf8_persian_ci NOT NULL,
-            `password` varchar(100) NOT NULL COLLATE utf8_persian_ci NOT NULL,
+            `id` int(11) NOT NULL AUTO_INCREMENT,
+            `username` varchar(100) NOT NULL COLLATE utf8_persian_ci,
+            `email` varchar(100) NOT NULL COLLATE utf8_persian_ci,
+            `password` varchar(100) NOT NULL COLLATE utf8_persian_ci ,
             `permission` enum('user','admin') COLLATE utf8_persian_ci NOT NULL DEFAULT 'user',
             `create_at` datetime NOT NULL,
-            `update_at` datetime DEFAULT,
+            `update_at` datetime DEFAULT NULL,
             PRIMARY KEY (`id`),
             UNIQUE KEY email (`email`)
         )ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_persian_ci;",
 
         "CREATE TABLE `articels` (
-            `id` int(11) NOT NULL AUTO_INCRIMENT,
+            `id` int(11) NOT NULL AUTO_INCREMENT,
             `title` varchar(200) COLLATE utf8_persian_ci NOT NULL,
             `summary` text COLLATE utf8_persian_ci NOT NULL,
             `body` text COLLATE utf8_persian_ci NOT NULL,
@@ -37,7 +37,7 @@ class CreateDB extends Database
             `image` varchar(200) COLLATE utf8_persian_ci NOT NULL,
             `status` enum('disable','enable') COLLATE utf8_persian_ci NOT NULL DEFAULT 'disable',
             `create_at` datetime NOT NULL,
-            `update_at` datetime DEFAULT,
+            `update_at` datetime DEFAULT NULL,
             PRIMARY KEY (`id`),
             FOREIGN KEY (`cat_id`) REFERENCES `categories` (`id`)  ON DELETE CASCADE ON UPDATE CASCADE,
             FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)  ON DELETE CASCADE ON UPDATE CASCADE
@@ -45,40 +45,41 @@ class CreateDB extends Database
         )ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_persian_ci;",
 
         "CREATE TABLE `comments` (
-            `id` int(11) NOT NULL AUTO_INCRIMENT,
+            `id` int(11) NOT NULL AUTO_INCREMENT,
             `user_id` int(11) NOT NULL ,
             `comment` text COLLATE utf8_persian_ci NOT NULL,
             `article_id` int(11) NOT NULL ,
             `status` enum('disable','enable') COLLATE utf8_persian_ci NOT NULL DEFAULT 'disable',
             `create_at` datetime NOT NULL,
-            `update_at` datetime DEFAULT,
+            `update_at` datetime DEFAULT NULL,
+            PRIMARY KEY (`id`),
             FOREIGN KEY (`article_id`) REFERENCES `articels` (`id`)  ON DELETE CASCADE ON UPDATE CASCADE,
             FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)  ON DELETE CASCADE ON UPDATE CASCADE
         )ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_persian_ci;",
 
         "CREATE TABLE `menus` (
-            `id` int(11) DEFAULT NULL AUTO_INCRIMENT,
+            `id` int(11) DEFAULT NULL AUTO_INCREMENT,
             `name` varchar(200) COLLATE utf8_persian_ci NOT NULL,
             `url` varchar(300) COLLATE utf8_persian_ci NOT NULL,
             `parent_id` int(11) NOT NULL ,
             `create_at` datetime DEFAULT NULL,
-            `update_at` datetime DEFAULT,
+            `update_at` datetime DEFAULT NULL,
             PRIMARY KEY (`id`),
             FOREIGN KEY (`parent_id`) REFERENCES `menus` (`id`)  ON DELETE CASCADE ON UPDATE CASCADE
 
         )ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_persian_ci;",
 
         "CREATE TABLE `websetting` (
-            `id` int(11) DEFAULT NULL AUTO_INCRIMENT,
+            `id` int(11) DEFAULT NULL AUTO_INCREMENT,
             `title` text COLLATE utf8_persian_ci DEFAULT NULL,
             `description` text COLLATE utf8_persian_ci DEFAULT NULL,
             `keywords` text COLLATE utf8_persian_ci DEFAULT NULL,
             `logo` text COLLATE utf8_persian_ci DEFAULT NULL,
             `icon` text COLLATE utf8_persian_ci DEFAULT NULL,
             `create_at` datetime DEFAULT NULL,
-            `update_at` datetime DEFAULT,
+            `update_at` datetime DEFAULT NULL,
             PRIMARY KEY (`id`)
-        )ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_persian_ci;",
+        )ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_persian_ci;"
 
         );
 
