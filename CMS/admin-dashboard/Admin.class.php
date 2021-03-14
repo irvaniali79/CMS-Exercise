7c9f34c7e1d9ce89741ba80150a8c22b72ed162a
okabe-rintaru <irvaniali79@gmail.com>
2021-03-14 22:10:19 +0330

    write admin class with redirect/back funcitons

---
+++ CMS/admin-dashboard/Admin.class.php
@@ -0,0 +1,15 @@
+<?php
+namespace AdminDashboard;
+class Admin{
+    protected function redirect($url)
+    {
+        $protocol=stripos( $_SERVER['SERVER_PROTOCOL'],'https')===true ? "https://":"http://";
+        header("Location:".$protocol.$_SERVER['HTTP_HOST']."/CMS".$url);
+
+
+    }
+    protected function redirectback(){
+        header("Location: ".$_SERVER['HTTP_REFERER']);
+    }
+
+}
