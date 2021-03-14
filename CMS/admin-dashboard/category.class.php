765b2b59d1695f49675560a4fe17517b88f80274
okabe-rintaru <irvaniali79@gmail.com>
2021-03-14 22:38:44 +0330

    write common category class structure

--- CMS/admin-dashboard/category.class.php
+++ CMS/admin-dashboard/category.class.php
@@ -1,2 +1,35 @@
 <?php
+namespace AdminDashboard;
 
+require_once("Admin.class.php");
+require_once(realpath(dirname(__FILE__)."/DataBase.php"));
+use DataBase\DataBase;
+
+class Category extends Admin{
+    public function index(){
+        
+    }
+    public function show($id){
+        
+    }
+
+    public function create(){
+
+    }
+    public function store($request)
+    {
+
+    } 
+    public function edit($id){
+
+    } 
+    public function update($id){
+
+    }
+    public function delete(){
+
+    }
+    
+
+
+}
