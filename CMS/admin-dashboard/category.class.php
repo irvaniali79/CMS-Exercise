c09a64bc1cc2df87de9e19535a0cff9e67f2f406
okabe-rintaru <irvaniali79@gmail.com>
2021-03-15 21:04:04 +0330

    add requires/redirects file in index show create edit delete store and update function--

--- CMS/admin-dashboard/category.class.php
+++ CMS/admin-dashboard/category.class.php
@@ -9,37 +9,41 @@ class Category extends Admin{
     public function index(){
         $db = new Database();
         $category= $db->select("SELECT * FROM `categories` ORDER BY `id` DESC;");
-
+        require_once(realpath(__FILE__)."../template/admin/categories/index.php");
     }
     public function show($id){
         $db = new Database();
         $category= $db->select("SELECT * FROM `categories` WHERE (`id` = ?);",[$id])->fetch();
+        require_once(realpath(__FILE__)."../template/admin/categories/show.php");
         
 
     }
 
     public function create(){
+        require_once(realpath(__FILE__)."../template/admin/categories/create.php");
         
     }
     public function store($request)
     {
         $db = new DataBase();
         $db->insert('categories',array_keys($request),array_values($request));
-
+        $this->redirect('category');
     } 
     public function edit($id){
         $db = new Database();
         $category= $db->select("SELECT * FROM `categories` WHERE (`id` = ?);",[$id])->fetch();
+        require_once(realpath(__FILE__)."../template/admin/categories/edit.php");
 
     } 
     public function update($request,$id){
         $db = new Database();
-        $db->update('categories',$id,array_keys($request),$request); 
+        $db->update('categories',$id,array_keys($request),$request);
+        $this->redirect('category');
     }
     public function delete($id){
         $db = new Database();
         $db->delete('categories',$id);
-
+        $this->redirectback();
     }
     
 
