d0ea491fd0b0a262f4a4cf78e7238503a776f18f
okabe-rintaru <irvaniali79@gmail.com>
2021-03-16 19:02:03 +0330

    implalement uri funciton

--- CMS/index.php
+++ CMS/index.php
@@ -4,8 +4,77 @@ require_once ("admin-dashboard/CreateDB.php");
 use AdminDashboard\Category;
 use DataBase\CreateDB;
 
+
+
 // $createDB= new CreateDB();
 // $createDB->run();
+
+
+function uri($uri,$class,$method,$requestMethod='GET')
+{
+    $values=array();
+    $subUris=explode('/',$uri);
+    $request_Uri=array_slice(explode('/',$_SERVER['REQUEST_URI']),2);
+    if($request_Uri[0]=='' or $request_Uri=='/'){
+        $request_Uri[0]='home';
+    }
+    $break=false;
+    if(sizeof($request_Uri)==sizeof($subUris) and $_SERVER['REQUEST_METHOD']==$requestMethod){
+        foreach(array_combine($subUris,$request_Uri) as $subUri=>$request){
+            if($subUri[0]=="{" and $subUri[strlen($subUri)-1]=="}"){
+                $values=array_push($values,$request);
+            }
+            else if ($subUri!=$request){
+                $break=true;
+                break;
+            }
+        }
+    }
+    else $break=true;
+
+    if(!$break)
+    {
+        $class="AdminDashboard\\".$class;
+        $object = new $class;
+        if(sizeof($values)>0){
+            if($requestMethod=="POST" ){
+                if( isset($_FILES)){
+                    $request=array_merge($_POST,$_FILES);
+                    $object->$method($request,implode(',',$values));
+
+                }
+                else
+                {
+                    $object->$method($_POST,implode(',',$values));
+
+                }  
+            }
+            else{
+                $object->$method(implode(',',$_POST));
+            }   
+
+        }
+        else{
+            if($requestMethod=="POST" ){
+                if(isset($_FILES)){
+                    $request=array_merge($_POST,$_FILES);
+                    $object->$method($request);
+
+                }
+                else{
+                    $object->$method($_POST);
+                }  
+            }   
+            else{
+                $object->$method;
+            }   
+        }
+
+    }
+    else{
+
+    }
+}
 
 //category Route
 
