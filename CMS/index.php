198a0fe824f3bdeb6a624050727829055e097dfc
okabe-rintaru <irvaniali79@gmail.com>
2021-03-16 11:14:13 +0330

    add category route with uri in index.php

--- CMS/index.php
+++ CMS/index.php
@@ -1,9 +1,22 @@
 <?php
+require_once ("admin-dashboard/category.class.php");
 require_once ("admin-dashboard/CreateDB.php");
+use AdminDashboard\Category;
 use DataBase\CreateDB;
 
 // $createDB= new CreateDB();
 // $createDB->run();
+
+//category Route
+
+uri('category','Category','index');
+uri('category/show/{id}','Category','show');
+uri('category/edit/{id}','Category','edit');
+uri('category/delete','Category','delete');
+uri('category/update/{id}','Category','update');
+uri('category/store','Category','store','POST');
+uri('category/create','Category','create');
+
 
 
 
