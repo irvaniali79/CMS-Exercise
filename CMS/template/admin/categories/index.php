0ee702300c638202a99336d294e40942ccc02903
okabe-rintaru <irvaniali79@gmail.com>
2021-03-17 23:40:08 +0330

    add update feature to template/index.php by write specified url

--- CMS/template/admin/categories/index.php
+++ CMS/template/admin/categories/index.php
@@ -26,7 +26,7 @@ require_once(realpath(dirname(__FILE__) . "/../layouts/head-tag.php"));
                 <td><?php echo $category['id'];?></td>
                 <td><?php echo $category['name'];?></td>
                 <td>
-                    <a role="button" href="#" class="btn btn-sm btn-info my-0 mx-1 text-white">update</a>
+                    <a role="button" href="http://localhost/www/CMS/category/edit/<?php echo $category['id']; ?>" class="btn btn-sm btn-info my-0 mx-1 text-white">update</a>
                     <a role="button" href="http://localhost/www/CMS/category/delete/<?php echo $category['id']; ?>" class="btn btn-sm btn-danger my-0 mx-1 text-white">delete</a>
                 </td>
             </tr>
