d8bdd62b0e0d7121367e61659e7e8ae5740412c7
okabe-rintaru <irvaniali79@gmail.com>
2021-03-17 23:48:26 +0330

    change edit form action in template/edit.php

--- CMS/template/admin/categories/edit.php
+++ CMS/template/admin/categories/edit.php
@@ -10,7 +10,7 @@ require_once(realpath(dirname(__FILE__) . "/../layouts/head-tag.php"));
 
 <section class="row my-3">
     <section class="col-12">
-        <form method="post" action="#">
+        <form method="post" action="http://localhost/www/CMS/category/update/<?php echo $id;?>">
             <div class="form-group">
                 <label for="name">Title</label>
                 <input type="text" class="form-control" id="name" name="name" placeholder="Enter name ..." value="<?php category['name']?>">
