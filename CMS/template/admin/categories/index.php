c71bc51b8c676cd476bbeb2838326f9802db37da
okabe-rintaru <irvaniali79@gmail.com>
2021-03-17 23:34:47 +0330

    add delete recordes feature in template/index.php

--- CMS/template/admin/categories/index.php
+++ CMS/template/admin/categories/index.php
@@ -27,7 +27,7 @@ require_once(realpath(dirname(__FILE__) . "/../layouts/head-tag.php"));
                 <td><?php $category['name']?></td>
                 <td>
                     <a role="button" href="#" class="btn btn-sm btn-info my-0 mx-1 text-white">update</a>
-                    <a role="button" href="#" class="btn btn-sm btn-danger my-0 mx-1 text-white">delete</a>
+                    <a role="button" href="http://localhost/www/CMS/category/delete/<?php $category['id']; ?>" class="btn btn-sm btn-danger my-0 mx-1 text-white">delete</a>
                 </td>
             </tr>
 <?php }?>
