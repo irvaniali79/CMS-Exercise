ba5e81e702b2eab42421ade211b14a7f982ed37f
okabe-rintaru <irvaniali79@gmail.com>
2021-03-19 00:27:03 +0330

    rename  Delete function to delete

--- CMS/admin-dashboard/DataBase.php
+++ CMS/admin-dashboard/DataBase.php
@@ -85,7 +85,7 @@ class DataBase
         }
                 
     }
-    public function Delete($tableName,$id)
+    public function delete($tableName,$id)
     {
         $sql="DELETE FROM ".$tableName." WHERE `id`=? ";
         try{
