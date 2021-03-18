190fd594b8fcd7e95caae0b9e0580822c481c8f8
okabe-rintaru <irvaniali79@gmail.com>
2021-03-18 16:41:09 +0330

    modify template/admin/index.php by adding href to A tag

--- CMS/template/admin/categories/index.php
+++ CMS/template/admin/categories/index.php
@@ -7,7 +7,7 @@ require_once(realpath(dirname(__FILE__) . "/../layouts/head-tag.php"));
     <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
         <h1 class="h5 "><i class="fas fa-newspaper"></i> Categories</h1>
         <div class="btn-toolbar mb-2 mb-md-0">
-            <a role="button" href="#" class="btn btn-sm btn-success">create</a>
+            <a role="button" href="http://localhost/www/CMS/category/create.php" class="btn btn-sm btn-success">create</a>
         </div>
     </div>
     <div class="table-responsive">
