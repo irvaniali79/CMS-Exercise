e2e5e04a56a0a54abb9b380f9f2988b7e835976f
okabe-rintaru <irvaniali79@gmail.com>
2021-03-21 09:32:29 +0330

    impelement saveimage function in admin.class

--- CMS/admin-dashboard/Admin.class.php
+++ CMS/admin-dashboard/Admin.class.php
@@ -12,4 +12,28 @@ class Admin{
         header("Location: ".$_SERVER['HTTP_REFERER']);
     }
 
+    protected function saveimage($image ,$imagePath ,$imageName=NULL){
+        if($imageName){
+            $imageName =$imageName .".".substr($image['type'],6,strlen($image["type"]));
+
+        }
+        else{
+            $imageName=date('Y-m-d-H-i-s').".".substr($image['type'],6,strlen($image['type']));
+        }
+        $imageTemp=$image['tmp_name'];
+        $imagePath="public/".$imagePath."/";
+        if(is_uploaded_file($imageTemp)){
+            if(move_uploaded_file($imageTemp,$imagePath.$imageName)){
+                return $imagePath.$imageName;
+            }
+            else{
+                return false;
+            }
+
+        }
+        else {
+            return false;
+        }
+
+    } 
 }
