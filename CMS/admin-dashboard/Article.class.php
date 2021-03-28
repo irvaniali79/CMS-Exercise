941d12a20917d6b766ea05302a3b26b0e088c68b
okabe-rintaru <irvaniali79@gmail.com>
2021-03-28 17:27:28 +0430

    modify store function

--- CMS/admin-dashboard/Article.class.php
+++ CMS/admin-dashboard/Article.class.php
@@ -1,83 +1,82 @@
 <?php
+
 namespace AdminDashboard;
-require_once ("Admin.class.php");
-require_once (realpath(dirname(__FILE__)."/DataBase.php"));
+
+require_once("Admin.class.php");
+require_once(realpath(dirname(__FILE__) . "/DataBase.php"));
 
 use DataBase\DataBase;
 
-class Article extends Admin{
+class Article extends Admin
+{
 
-    public function index(){
+    public function index()
+    {
         $db = new Database();
-        $articles= $db->select("SELECT * FROM `articles` ORDER BY `id` DESC;");
-        require_once (realpath(dirname(__FILE__)."/../template/admin/articles/index.php"));
-        
+        $articles = $db->select("SELECT * FROM `articles` ORDER BY `id` DESC;");
+        require_once(realpath(dirname(__FILE__) . "/../template/admin/articles/index.php"));
     }
-    public function show($id){
+    public function show($id)
+    {
         $db = new Database();
-        $article= $db->select("SELECT * FROM `articles` WHERE (`id` = ?);",[$id])->fetch();
-        require_once (realpath(dirname(__FILE__)."/../template/admin/articles/show.php"));
-        
-
+        $article = $db->select("SELECT * FROM `articles` WHERE (`id` = ?);", [$id])->fetch();
+        require_once(realpath(dirname(__FILE__) . "/../template/admin/articles/show.php"));
     }
 
-    public function create(){
-        $db =new Database();
-        $categories= $db->select("SELECT * FROM `categories` ORDER BY `id` DESC;");
-        require_once (realpath(dirname(__FILE__)."/../template/admin/articles/create.php"));   
+    public function create()
+    {
+        $db = new Database();
+        $categories = $db->select("SELECT * FROM `categories` ORDER BY `id` DESC;");
+        require_once(realpath(dirname(__FILE__) . "/../template/admin/articles/create.php"));
     }
     public function store($request)
     {
         $db = new DataBase();
-        if($request['cat_id']!=null){
-            $request['image']=$this->saveimage($request['image'],'article-image');
-            if($request['image']){
-                $request=array_merge($request,array('user_id'=>$_SESSION['user']));
-                $db->insert('articles',array_keys($request),array_values($request));
+        if ($request['cat_id'] != null) {
+            $request['image'] = $this->saveimage($request['image'], 'article-image');
+            if ($request['image']) {
+                $request = array_merge($request, array('user_id' => $_SESSION['user']));
+                $db->insert('articles', array_keys($request), array_values($request));
                 $this->redirect('article');
-            }
-            else $this->redirectback();
-        }
-        else{
+            } else $this->redirectback();
+        } else {
             $this->redirectback();
         }
 
-        
-        $this->redirect('article');
-    } 
-    public function edit($id){
-        $db = new Database();
-        $article= $db->select("SELECT * FROM `articles` WHERE `id` = ?;",[$id])->fetch();
-        $categories= $db->select("SELECT * FROM `categories` ORDER BY `id` DESC;");
-        require_once (realpath(dirname(__FILE__)."/../template/admin/articles/edit.php"));
 
-    } 
-    public function update($request,$id){
+        $this->redirect('article');
+    }
+    public function edit($id)
+    {
+        $db = new Database();
+        $article = $db->select("SELECT * FROM `articles` WHERE `id` = ?;", [$id])->fetch();
+        $categories = $db->select("SELECT * FROM `categories` ORDER BY `id` DESC;");
+        require_once(realpath(dirname(__FILE__) . "/../template/admin/articles/edit.php"));
+    }
+    public function update($request, $id)
+    {
         $db = new DataBase();
-        if($request['cat_id']!=null){
-            $request['image']=$this->saveimage($request['image'],'article-image');
-            if($request['image']){
-                $request=array_merge($request,array('user_id'=>$_SESSION['user']));
-                $db->insert('articles',array_keys($request),array_values($request));
-                $this->redirect('article');
-            }
-            else{
+        if ($request['cat_id'] != null) {
+            if ($request['image']['tmp_name'] != null) {
+                $article = $db->select("SELECT * FROM `articles` WHERE `id` = ?;", [$id])->fetch();
+                $this->removeimage($article['image']);
+                $request['image'] = $this->saveimage($request['image'], 'article-image');
+            } else {
                 unset($request['image']);
             }
-            $request= array_merge($request,array('user_id'=>$_SESSION['user']));
-            $db->update('articles',$id,array_keys($request),$request);
+            $request = array_merge($request, array('user_id' => $_SESSION['user']));
+            $db->update('articles', $id, array_keys($request), $request);
             $this->redirect('article');
-        }
-        else{
+        } else {
             $this->redirectback();
         }
     }
-    public function delete($id){
+    public function delete($id)
+    {
         $db = new Database();
-        $article=$db->select("SELECT * FROM `articles` WHERE `id` = ? ;",[$id])->fetch();
+        $article = $db->select("SELECT * FROM `articles` WHERE `id` = ? ;", [$id])->fetch();
         $this->removeimage($article['image']);
-        $db->delete('articles',$id);
+        $db->delete('articles', $id);
         $this->redirectback();
     }
-
 }
