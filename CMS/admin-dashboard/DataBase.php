13584277611806b1b5a7d572f987a31f856f5cc2
okabe-rintaru <irvaniali79@gmail.com>
2021-03-28 13:29:47 +0430

    add pdo option "utf8"

--- CMS/admin-dashboard/DataBase.php
+++ CMS/admin-dashboard/DataBase.php
@@ -8,7 +8,7 @@ class DataBase
 {
     private $connection;
     private $options=array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE=>PDO::FETCH_ASSOC);
+    PDO::ATTR_DEFAULT_FETCH_MODE=>PDO::FETCH_ASSOC,PDO::MYSQL_ATTR_INIT_COMMAND=> "SET NAMES utf8");
     private $dbHost='127.0.0.1';
     private $dbName='mycms';
     private $dbUsername='root';
