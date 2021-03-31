05c6126beb2c08034f67d5f71cdbfbdc6e64f415
okabe-rintaru <irvaniali79@gmail.com>
2021-03-31 11:59:12 +0430

    fixing typo mistakes and changing some fetches to fetchAll

--- CMS/admin-dashboard/Dashboard.class.php
+++ CMS/admin-dashboard/Dashboard.class.php
@@ -11,19 +11,20 @@ class Dashboard extends Admin{
 
         $db=new DataBase;
         $articleCount=$db->select('SELECT COUNT(*) FROM `articles` ;')->fetch();
-        $articleView=$db->select('SELECT SUM(view) FROM `articles` ;')->fetch();
+        $articlesViews=$db->select('SELECT SUM(view) FROM `articles` ;')->fetch();
         $commentsCount=$db->select('SELECT COUNT(*) FROM `comments` ;')->fetch();
-        $commentsApproveCount=$db->select('SELECT COUNT(*) FROM `comments` WHERE `status`=`unseen`  ;')->fetch();
-        $userCount=$db->select('SELECT COUNT(*) FROM `users` WHERE `premission` = `user` ;')->fetch();
-        $adminCount=$db->select('SELECT COUNT(*) FROM `users` WHERE `premission` = `admin` ;')->fetch();
-        $categoryCount=$db->select('SELECT * FROM `categories` ;');
-        $articleWithView=$db->select('SELECT COUNT(*) FROM `articles` ORDER BY `view` DESC LIMIT 0,5')->fetch();
-        $articlesComments=$db->select("SELECT `articles`.`id` ,`articles`.`title`,COUNT(`comments`.`article_id`) AS `comment_count` FROM `articles` LEFT JOIN `comments` ON `articles`.`id` = `comments`.`article_id` GROUP BY `articles`.`id` ORDER BY `comment_count` DESC LIMIT 0,5;")->fetch();
-        $lastComments=$db->select("SELECT `comments`.`id`,`comments`.`comment`,`comments`.`status`,`comments`.`article_id`,`users`.`username` FROM comments,users WHERE `comments`.`user_id`=`user`.`id` ORDER BY `comments`.`created_at` DESC LIMIT 0,5;")->fetch();
+        $commentsApproveCount=$db->select('SELECT COUNT(*) FROM `comments` WHERE `comments`.`status`="approve"  ;')->fetch();
+        $commentsUnseenCount=$db->select('SELECT COUNT(*) FROM `comments` WHERE `comments`.`status`="unseen"  ;')->fetch();
+        $userCount=$db->select('SELECT COUNT(*) FROM `users` WHERE `permission` = "user" ;')->fetch();
+        $adminCount=$db->select('SELECT COUNT(*) FROM `users` WHERE `permission` = "admin" ;')->fetch();
+        $categoryCount=$db->select('SELECT COUNT(*) FROM `categories` ;')->fetch();
+        $articlesWithView=$db->select('SELECT * FROM `articles` ORDER BY `view` DESC LIMIT 0,5')->fetchAll();
+        $articlesComments=$db->select("SELECT `articles`.`id` ,`articles`.`title`,COUNT(`comments`.`article_id`) AS `comment_count` FROM `articles` LEFT JOIN `comments` ON `articles`.`id` = `comments`.`article_id` GROUP BY `articles`.`id` ORDER BY `comment_count` DESC LIMIT 0,5;")->fetchAll();
+        $lastComments=$db->select("SELECT `comments`.`id`,`comments`.`comment`,`comments`.`status`,`comments`.`article_id`,`users`.`username` FROM comments,users WHERE `comments`.`user_id`=`users`.`id` ORDER BY `comments`.`created_at` DESC LIMIT 0,5;")->fetchAll();
         
         
 
-        require_once (realpath(dirname(__FILE__)."/../CMS/template/admin/dashboard/index.php"));
+        require_once (realpath(dirname(__FILE__)."/../template/admin/dashboard/index.php"));
     }
    
 }
