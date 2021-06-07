0bf94fa37da8e131e3255b96f232dd55a50748d6
Ali irvani <66170668+okabe-rintaru@users.noreply.github.com>
2021-06-07 14:16:19 +0430

    changing addresses

--- CMS/Controllers/WebSetting.class.php
+++ CMS/Controllers/WebSetting.class.php
@@ -1,18 +1,19 @@
 <?php
 namespace AdminDashboard;
 require_once ("Admin.class.php");
-require_once (realpath(dirname(__FILE__)."/DataBase.php"));
+require_once(realpath(dirname(__FILE__) . "/../vendor/database/DataBase.php"));
+
 use DataBase\DataBase;
 class WebSetting extends Admin {
     public function index(){
         $db=new DataBase();
         $setting=$db->select("SELECT * FROM `websetting` ;")->fetch();
-        require_once (realpath(dirname(__FILE__)."/../template/admin/web-setting/index.php"));
+        require_once (realpath(dirname(__FILE__)."/../view/admin/web-setting/index.php"));
     }
     public function set(){
         $db=new DataBase();
         $setting=$db->select("SELECT * FROM `websetting` ;")->fetch();
-        require_once (realpath(dirname(__FILE__)."/../template/admin/web-setting/set.php"));
+        require_once (realpath(dirname(__FILE__)."/../view/admin/web-setting/set.php"));
     }
     public function store($request){
         $db=new DataBase();
