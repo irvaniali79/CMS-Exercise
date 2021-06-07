75b2b73c0378dddb1ac869c5cf54bb951a8977ad
Ali irvani <66170668+okabe-rintaru@users.noreply.github.com>
2021-06-07 14:17:06 +0430

    changing addresses

--- CMS/Controllers/category.class.php
+++ CMS/Controllers/category.class.php
@@ -2,7 +2,8 @@
 namespace AdminDashboard;
 
 require_once("Admin.class.php");
-require_once(realpath(dirname(__FILE__)."/DataBase.php"));
+require_once(realpath(dirname(__FILE__) . "/../vendor/database/DataBase.php"));
+
 use DataBase\DataBase;
 
 
@@ -10,19 +11,19 @@ class Category extends Admin{
     public function index(){
         $db = new Database();
         $categories= $db->select("SELECT * FROM `categories` ORDER BY `id` DESC;");
-        require_once (realpath(dirname(__FILE__)."/../template/admin/categories/index.php"));
+        require_once (realpath(dirname(__FILE__)."/../view/admin/categories/index.php"));
         
     }
     public function show($id){
         $db = new Database();
         $category= $db->select("SELECT * FROM `categories` WHERE (`id` = ?);",[$id])->fetch();
-        require_once (realpath(dirname(__FILE__)."/../template/admin/categories/show.php"));
+        require_once (realpath(dirname(__FILE__)."/../view/admin/categories/show.php"));
         
 
     }
 
     public function create(){
-        require_once (realpath(dirname(__FILE__)."/../template/admin/categories/create.php"));
+        require_once (realpath(dirname(__FILE__)."/../view/admin/categories/create.php"));
         
     }
     public function store($request)
@@ -34,7 +35,7 @@ class Category extends Admin{
     public function edit($id){
         $db = new Database();
         $category= $db->select("SELECT * FROM `categories` WHERE `id` = ?;",[$id])->fetch();
-        require_once (realpath(dirname(__FILE__)."/../template/admin/categories/edit.php"));
+        require_once (realpath(dirname(__FILE__)."/../view/admin/categories/edit.php"));
 
     } 
     public function update($request,$id){
